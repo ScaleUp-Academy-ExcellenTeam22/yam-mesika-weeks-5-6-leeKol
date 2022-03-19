d0a466e80256f711c8c30327326a110f1024862b
Lee Kol <lee.levy333@gmail.com>
2022-03-19 21:58:14 +0200

    The this_is_the_way exercise is done.

--- this_is_the_way.py
+++ this_is_the_way.py
@@ -1,1 +1,12 @@
-print("hello")
+import glob
+import os
+
+
+def get_deep_files(path):
+    """
+    The function gets a path to a directory, and returns a list of all the files
+    that start with "deep" in the directory.
+    :param path: The path of the directory we want to extract the information from.
+    :return: A list of all the files that start with "deep" in the directory.
+    """
+    return [os.path.basename(file) for file in glob.glob(path + "/deep*")]
